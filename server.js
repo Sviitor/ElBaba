40c883cc4e65f577cd2faae3c43589b9db321e37
Sviitor <108687219+Sviitor@users.noreply.github.com>
2023-12-09 15:20:47 -0300

    Update server.js

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ const normalizePort = val => {
     console.log( " Listening on " + bind);  
   };  
 
-const port = normalizePort(process.env.PORT || "4000"); 
+const port = normalizePort(process.env.PORT || "8000"); 
 app.set('port', port);
 const server = http.createServer(app);  
 server.on("error", onError);  
